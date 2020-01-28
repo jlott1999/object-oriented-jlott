47ac6f3272dad0ef09e035057b0885c78fcf4737
Jacob Lott <58454146+jlott1999@users.noreply.github.com>
2020-01-28 09:22:58 -0700

    Added more content!!!!!!!!!!!!!!!

---
+++ php/Ramsey/Uuid/Uuid.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Ramsey\Uuid;
+
+
+class Uuid
+{
+
+    public static function isValid(Uuid $newUuid)
+    {
+    }
+
+    public static function fromString(Uuid $newUuid)
+    {
+    }
+}
---
+++ php/jlott3/Captain.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace jlott3;
+
+
+class Captain
+{
+
+    /**
+     * Captain constructor.
+     * @param string $string
+     * @param int $int
+     */
+    public function __construct(string $string, int $int)
+    {
+    }
+}
